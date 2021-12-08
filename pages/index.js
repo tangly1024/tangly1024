a10a6e856c8242acf31f373a4568d89739281b83
tangly1024 <tlyong1992@hotmail.com>
2021-12-08 10:53:25 +0800

    feature: 加入不蒜子

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -9,16 +9,16 @@ export default function Home() {
         <title>塘里</title>
         <meta name="description" content="唐风集里，收卷波澜" />
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src={"//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"}
+        />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          唐风集里，收卷波澜
-        </h1>
+        <h1 className={styles.title}>唐风集里，收卷波澜</h1>
 
-        <p className={styles.description}>
-          在这里一起编程、一起思考
-        </p>
+        <p className={styles.description}>在这里一起编程、一起思考</p>
 
         <div className={styles.grid}>
           <a href="https://blog.tangly1024.com" className={styles.card}>
@@ -39,21 +39,35 @@ export default function Home() {
             <p>一个程序员的自我独白，对酒当歌，人生几何</p>
           </a>
 
-          <a
-            href="https://github.com/tangly1024"
-            className={styles.card}
-          >
+          <a href="https://github.com/tangly1024" className={styles.card}>
             <h2>Github &rarr;</h2>
-            <p>
-              我的代码都在这里开源，欢迎Star和Fork
-            </p>
+            <p>我的代码都在这里开源，欢迎Star和Fork</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-      <a href='https://beian.miit.gov.cn/' className='ml-1 font-bold'>闽ICP备20010331号</a>
+        <a href="https://beian.miit.gov.cn/" className="ml-1 font-bold">
+          闽ICP备20010331号
+        </a>
+        <br />
       </footer>
+      <div>
+        <span id="busuanzi_container_site_pv" style={{ display: "none" }}>
+          总访问量
+          <span id="busuanzi_value_site_pv" className="px-1">
+            {" "}
+          </span>
+          &nbsp;次&nbsp;
+        </span>
+        <span id="busuanzi_container_site_uv" style={{ display: "none" }}>
+          总访客数
+          <span id="busuanzi_value_site_uv" className="px-1">
+            {" "}
+          </span>
+          &nbsp;人
+        </span>
+      </div>
     </div>
-  )
+  );
 }
