aae4a803b911b26b946552db82644728b7203115
tangly1024 <tlyong1992@hotmail.com>
2021-12-10 15:57:09 +0800

    清空代码

--- components/login/Page.js
+++
@@ -1,7 +0,0 @@
-export default function Page({ children }) {
-    return (
-        <div className="bg-login-background bg-cover bg-center w-screen h-screen relative flex flex-col justify-between">
-            {children}
-        </div>
-    );
-}
